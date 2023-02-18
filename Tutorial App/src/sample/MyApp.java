0065895aa20fa114e78e09cde041a97906130e30
MD Rakib Ahsan <79665060+Rakib4442@users.noreply.github.com>
2023-02-18 10:51:55 +0600

    Add files via upload

---
+++ Tutorial App/src/sample/MyApp.java
@@ -0,0 +1,98 @@
+package sample;
+import org.openqa.selenium.By; 
+ 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class MyApp {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ahsan\\Downloads\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		 // Navigate to the website
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize(); 
+        
+        Thread.sleep(4000);
+
+        // Find the username field and enter "standard_user"
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+
+        // Find the password field and enter "secret_sauce"
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        // Click the login button
+        driver.findElement(By.id("login-button")).click();
+        
+        Thread.sleep(4000);
+
+        // Find the "Sauce Labs Backpack" product and click the "Add to Cart" button
+        driver.findElement(By.xpath("//div[text()='Sauce Labs Backpack']/ancestor::div[@class='inventory_item']//button"))
+              .click();
+        
+        Thread.sleep(4000);
+
+        // Find the "Sauce Labs Fleece Jacket" product and click the "Add to Cart" button
+        driver.findElement(By.xpath("//div[text()='Sauce Labs Fleece Jacket']/ancestor::div[@class='inventory_item']//button"))
+              .click();
+        
+        Thread.sleep(4000);
+
+        // Click the shopping cart icon to go to the cart page
+        driver.findElement(By.className("shopping_cart_container")).click();
+        
+        Thread.sleep(4000);
+
+        // Click the "CHECKOUT" button to go to the "Checkout: Your Information" page
+        driver.findElement(By.className("checkout_button")).click();
+        
+        Thread.sleep(4000);
+
+        // Find the first name field and enter "John"
+        driver.findElement(By.id("first-name")).sendKeys("John");
+
+        // Find the last name field and enter "Doe"
+        driver.findElement(By.id("last-name")).sendKeys("Doe");
+
+        // Find the zip code field and enter "12345"
+        driver.findElement(By.id("postal-code")).sendKeys("12345");
+        
+        Thread.sleep(4000);
+
+        // Click the "CONTINUE" button to go to the "Checkout: Overview" page
+        driver.findElement(By.className("cart_button")).click();
+        
+        Thread.sleep(4000);
+
+        // Click the "FINISH" button to complete the order
+        driver.findElement(By.className("cart_button")).click();
+        
+        Thread.sleep(4000);
+
+        // Close the browser
+        driver.quit();
+		
+
+		//driver.navigate().to("https://book.spicejet.com/search.aspx");  
+		   //driver.manage().window().maximize();  
+		   
+		   //JavascriptExecutor js = (JavascriptExecutor)driver;  
+	        //js.executeScript("scrollBy(0, 5000)");  
+
+	        //driver.findElement(By.linkText(""))
+	        
+	    
+	        //Thread.sleep(4000);
+
+	        
+//	        Select drpCountry = new Select(driver.findElement(By.name("country")));
+//			drpCountry.selectByVisibleText("ANTARCTICA");
+//			
+			
+	}		
+	
+
+}
